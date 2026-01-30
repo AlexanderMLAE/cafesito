474c530cb934c830b7e008d57fc3a4e6201878c8
= <233278825+AlexanderMLAE@users.noreply.github.com>
2026-01-30 17:53:09 -0500

    Clase 2026-30-01, uso de useEffect y useState

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,14 @@
 import './App.css'
 import Login from './components/Login'
 import Quotes from './components/Quotes'
+import Dashboard from './components/Dashboard'
 function App() {
 
   return (
     <>
       <h1>Hello Ipsum!</h1>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse, ex id necessitatibus similique corporis ut dolor explicabo quam maxime itaque!</p>
-    <Login/>
-    <Quotes/>
+      <Dashboard/>
     </>
   )
 }
---
+++ src/components/Dashboard.jsx
@@ -0,0 +1,47 @@
+import axios from "axios"
+import { useEffect, useState} from "react"
+
+function Dashboard() {
+	const url = 'https://dummyjson.com/quotes'
+	// HOOKS
+	const [quotes, setQuotes ] = useState([])
+	const [loading, setLoading ] = useState(true)
+	// INVOCAR EL SERVICIO
+	const getQuotes=async ()=>{
+		try {
+			const result = await axios.get(url)
+			console.log(result.data.quotes)
+			setQuotes(result.data.quotes)
+			setLoading(false)
+	}
+		catch(error) {
+			console.error("Error al solicitar los datos", error)	
+			setLoading(true)
+	
+		}
+	}
+	useEffect(() => {
+		getQuotes()
+	},[])
+
+	return (
+		<>
+			<h1>Dashboard</h1>
+			{loading ? <img src="https://www.drupal.org/files/issues/throbber_12.gif"/> : (
+				<>
+				<p>Mostrando datos</p>
+				{
+					quotes.map(
+						item=>(
+							<p> {item.quote} - {item.author} </p>
+						)
+					)
+				}
+				</>	
+			)
+			}
+		</>
+	)
+}
+
+export default Dashboard
