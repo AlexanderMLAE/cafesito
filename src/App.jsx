2174703739b1a0530a754eaa3a45de9186b3af72
AlexanderMLAE <233278825+AlexanderMLAE@users.noreply.github.com>
2026-02-04 08:20:12 -0500

    Made map a separate component

--- src/App.jsx
+++ src/App.jsx
@@ -1,73 +1,16 @@
 // Imports
-
 import './App.css'
-import Login from './components/Login'
-import Quotes from './components/Quotes'
 import Dashboard from './components/Dashboard'
-import { useRef, useEffect, useState } from "react"
-import mapboxgl from 'mapbox-gl'
 import 'mapbox-gl/dist/mapbox-gl.css'
-
+import Map from './components/Map'
 // Declarations
-
-const INITIAL_CENTER = [
-  139.68974,
-  35.69812
-]
-
-const INITIAL_ZOOM = 7.96
-
 function App() {
-
-  const mapRef = useRef()
-  const mapContainerRef = useRef()
-
-  const [center, setCenter] = useState(INITIAL_CENTER)
-  const [zoom, setZoom] = useState(INITIAL_ZOOM)
-
-  useEffect(() => {
-    mapboxgl.accessToken = 'pk.eyJ1IjoiYWxleGFuZGVyLW1sYWUiLCJhIjoiY21sMms0c3ZlMGtkbTNkb20wZGJsajBwaCJ9.i-X4cyxRbNJn5Y5gEnpRIQ'
-    mapRef.current = new mapboxgl.Map({
-      container: mapContainerRef.current,
-      center: center,
-      zoom: zoom,
-    });
-
-    mapRef.current.on('move', () => {
-      // get the current center coordinates and zoom level from the map
-      const mapCenter = mapRef.current.getCenter()
-      const mapZoom = mapRef.current.getZoom()
-
-      // update state
-      setCenter([mapCenter.lng, mapCenter.lat])
-      setZoom(mapZoom)
-    })
-
-    return () => {
-      mapRef.current.remove()
-    }
-  }, [])
-
-  const handleButtonClick = () => {
-    mapRef.current.flyTo({
-      center: INITIAL_CENTER,
-      zoom: INITIAL_ZOOM
-    })
-  }
 
   return (
     <>
       <h1>Hello Ipsum!</h1>
       <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Esse, ex id necessitatibus similique corporis ut dolor explicabo quam maxime itaque!</p>
-      
-      <div className="sidebar">
-        Longitude: {center[0].toFixed(4)} | Latitude: {center[1].toFixed(4)} | Zoom: {zoom.toFixed(2)}
-      </div>
-      <button className='reset-button' onClick={handleButtonClick}>
-        Reset
-      </button>
-      <div id='map-container' ref={mapContainerRef} />
-      
+      <Map/>
       <Dashboard />
     </>
   )
---
+++ src/components/Map.jsx
@@ -0,0 +1,68 @@
+// Imports
+
+import { useRef, useEffect, useState } from "react"
+import mapboxgl from 'mapbox-gl'
+import 'mapbox-gl/dist/mapbox-gl.css'
+
+// Declarations
+
+const INITIAL_CENTER = [-86.84692, 21.04851]
+
+const INITIAL_ZOOM = 13 
+
+function Map() {
+
+    const mapRef = useRef()
+    const mapContainerRef = useRef()
+
+    const [center, setCenter] = useState(INITIAL_CENTER)
+    const [zoom, setZoom] = useState(INITIAL_ZOOM)
+
+    useEffect(() => {
+        mapboxgl.accessToken = 'pk.eyJ1IjoiYWxleGFuZGVyLW1sYWUiLCJhIjoiY21sMms0c3ZlMGtkbTNkb20wZGJsajBwaCJ9.i-X4cyxRbNJn5Y5gEnpRIQ'
+        mapRef.current = new mapboxgl.Map({
+            container: mapContainerRef.current,
+            center: center,
+            zoom: zoom,
+
+        });
+        new mapboxgl.Marker()
+            .setLngLat([12.554729, 55.70651])
+            .addTo(mapRef.current);
+
+        mapRef.current.on('move', () => {
+            // get the current center coordinates and zoom level from the map
+            const mapCenter = mapRef.current.getCenter()
+            const mapZoom = mapRef.current.getZoom()
+
+            // update state
+            setCenter([mapCenter.lng, mapCenter.lat])
+            setZoom(mapZoom)
+        })
+
+        return () => {
+            mapRef.current.remove()
+        }
+    }, [])
+
+    const handleButtonClick = () => {
+        mapRef.current.flyTo({
+            center: INITIAL_CENTER,
+            zoom: INITIAL_ZOOM
+        })
+    }
+
+    return (
+        <>
+            <div className="sidebar">
+                Longitude: {center[0].toFixed(4)} | Latitude: {center[1].toFixed(4)} | Zoom: {zoom.toFixed(2)}
+            </div>
+            <button className='reset-button' onClick={handleButtonClick}>
+                Reset
+            </button>
+            <div id='map-container' ref={mapContainerRef} />
+        </>
+    )
+}
+
+export default Map 
